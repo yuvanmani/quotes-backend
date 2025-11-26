61ff4acc2f5c632ef78f676a99c59034cb955f89
Manivannan <yuvanmani007@gmail.com>
2025-11-26 13:13:51 +0530

    brevo email provider changed

--- utils/config.js
+++ utils/config.js
@@ -5,11 +5,17 @@ const EMAIL_USER = process.env.EMAIL_USER;
 const EMAIL_PASS = process.env.EMAIL_PASS;
 const JWT_SECRET = process.env.JWT_SECRET;
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+const BREVO_API_KEY = process.env.BREVO_API_KEY;
+const SENDER_NAME = process.env.SENDER_NAME;
+const SENDER_EMAIL = process.env.SENDER_EMAIL;
 
 module.exports = {
     MONGODB_URI,
     EMAIL_USER,
     EMAIL_PASS,
     JWT_SECRET,
-    SENDGRID_API_KEY
+    SENDGRID_API_KEY,
+    BREVO_API_KEY,
+    SENDER_NAME,
+    SENDER_EMAIL
 }
--- utils/emailService.js
+++ utils/emailService.js
@@ -1,26 +1,37 @@
-const { EMAIL_USER, SENDGRID_API_KEY } = require("../utils/config");
-const sgMail = require("@sendgrid/mail");
-
-sgMail.setApiKey(SENDGRID_API_KEY);
+const { SENDER_EMAIL, BREVO_API_KEY, SENDER_NAME } = require("../utils/config");
+const axios = require("axios");
 
 // create a sendEmail function to send OTP via email
 const sendEmail = async (to, subject, text) => {
     try {
 
-        // setup mail data
-        const msg = {
-            from: EMAIL_USER,
-            to: to,
-            subject: subject,
-            text: text
-        }
+        const response = await axios.post("https://api.brevo.com/v3/smtp/email",
+            {
+                sender: {
+                    name: SENDER_NAME,
+                    email: SENDER_EMAIL
+                },
+                to: [
+                    {
+                        email: to
+                    }
+                ],
+                subject: subject,
+                textContent: text
+            },
+            {
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "api-key": BREVO_API_KEY
+                }
+            }
+        );
 
-        // send the email
-        const info = await sgMail.send(msg);
-        console.log("Email sent : ", info[0].statusCode);
+        console.log("Email sent : ", response.data);
     }
     catch (error) {
-        console.log("Error sending email", error);
+        console.error("Error sending email", error.response ? error.response.data : error.message);
     }
 }
 
